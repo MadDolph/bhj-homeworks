bca3c211627a9f7533d170fbbbdb9b722ecfc250
MadDolph <kurbatov@it-matika.pro>
2023-02-13 21:35:52 +0300

    2 2#	modified:   element-search/menu/task.js

---
+++ element-search/menu/task.js
@@ -0,0 +1,7 @@
+const menu_item = document.getElementsByClassName("menu__link");
+let menu_array = Array.from(menu_item);
+for(let i = 0; i < menu_array.length; i++) {
+    menu_array[i].onclick = () => {
+        
+    }
+}

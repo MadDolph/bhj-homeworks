c42cbcf89eefc4ea468e023a59e09883a83c131f
MadDolph <kurbatov@it-matika.pro>
2023-02-14 01:34:37 +0300

    SecondTaskComplete

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,2 +1,13 @@
-const start = document.getElementById('modal_main');
-start.classList.add('modal_active');
+const modalMain = document.getElementById('modal_main');
+modalMain.classList.add('modal_active');
+const closure = document.querySelectorAll('.modal__close');
+let onclickArray = Array.from(closure);
+for(let i=0; i < onclickArray.length; i++) {
+    onclickArray[i].onclick = () => {        
+        const active = document.querySelector('.modal_active');
+        active.classList.remove('modal_active');
+        if(onclickArray[i].classList.contains('show-success')) {
+            document.getElementById('modal_success').classList.add('modal_active');
+        }
+    }
+}

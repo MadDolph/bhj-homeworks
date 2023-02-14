208c13cca203149c35e626ef0d8bc81667f7904a
MadDolph <kurbatov@it-matika.pro>
2023-02-14 03:33:07 +0300

    additional for task 1.3 complete

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,6 +1,8 @@
 const rightButton = document.querySelector('.slider__arrow_next');
 const leftButton = document.querySelector('.slider__arrow_prev');
 const slides = document.querySelectorAll('.slider__item');
+const dots = document.getElementsByClassName('slider__dot');
+let dotsArray = Array.from(dots);
 let slidesArray = Array.from(slides);
 let activeSlide = 0;
 rightButton.onclick = () => {
@@ -10,14 +12,14 @@ leftButton.onclick = () => {
     leaf("left");
 }
 function leaf(direction) {
-    slidesArray[activeSlide].classList.remove('slider__item_active');
+    slideManager("hid", activeSlide);   
     switch(direction) {
         case "right":
             activeSlide++;
             break;
         case "left":
             activeSlide--;
-            break
+            break;
     }
     if(activeSlide == slidesArray.length) {
         activeSlide = 0;
@@ -25,6 +27,24 @@ function leaf(direction) {
     if(activeSlide < 0) {
         activeSlide = slidesArray.length - 1;
     }
-    slidesArray[activeSlide].classList.add('slider__item_active');
+    slideManager("show", activeSlide);
+    return;
+}
+for(let i = 0; i < dotsArray.length; i++) {
+    dotsArray[i].onclick = () => {
+        slidesArray[activeSlide].classList.remove('slider__item_active');
+        activeSlide = i;
+        slidesArray[activeSlide].classList.add('slider__item_active');
+    }
+}
+function slideManager(flag, id) {
+    switch(flag) {
+        case "show":
+            slidesArray[id].classList.add('slider__item_active');
+            break;
+        case "hid":
+            slidesArray[id].classList.remove('slider__item_active');
+            break;
+    }
     return;
 }

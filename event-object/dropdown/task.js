fc03ce9cc630803afb89321461fcbfe50afdf617
MadDolph <kurbatov@it-matika.pro>
2023-02-18 00:55:10 +0300

    fist task

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,8 +1,18 @@
-const button = document.querySelector('.dropdown__value');
+const button = document.querySelector('.dropdown');
 const menu = document.querySelector('.dropdown__list');
+const menuItem = document.getElementsByClassName("dropdown__item");
+let itemArray = Array.from(menuItem);
 menu.drop = false;
-button.addEventListener('click', drop_collapse);
-
+button.addEventListener('click', e => {
+    if(e.target.className === 'dropdown__value') {
+        drop_collapse();
+    }
+    if(e.target.className === 'dropdown__link') {
+        button.querySelector('.dropdown__value').textContent = e.target.textContent;
+        drop_collapse();
+    }
+    e.preventDefault();
+});
 function drop_collapse() {
     if(!menu.drop) {
         menu.classList.add('dropdown__list_active');
@@ -10,11 +20,6 @@ function drop_collapse() {
     } else {
         menu.classList.remove('dropdown__list_active');
         menu.drop = false;
-    }
+    }        
     return false;
 }
-
-
-
-
-

0eb2d96c5aca648e6d6c2249242039e3f62f4b78
MadDolph <kurbatov@it-matika.pro>
2023-02-18 23:49:55 +0300

    Second task complete

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,13 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+    addEventListener("keypress", e => {
+      if(this.currentSymbol.innerText.toUpperCase() === e.key.toUpperCase()) {
+        this.success();
+      } else {
+        this.fail();
+      }
+    })   
   }
 
   success() {

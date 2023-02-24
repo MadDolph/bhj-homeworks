89ee8eb6a3721cea052cafc436e6e6a316ccc6e2
MadDolph <kurbatov@it-matika.pro>
2023-02-25 01:24:16 +0300

    Remarks added

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,5 +1,12 @@
 const rev = document.querySelectorAll('.reveal');
 const windowPointer = window;
-windowPointer.addEventListener('scroll', (e) => {
-  console.log("scroll");
+let revArray = Array.from(rev);
+windowPointer.addEventListener('scroll', () => {
+    for(let i = 0; i < revArray.length; i++) {
+        if(revArray[i].getBoundingClientRect().bottom > 0 && revArray[i].getBoundingClientRect().top < windowPointer.innerHeight) {
+            revArray[i].classList.add('reveal_active');
+        }  else {
+            revArray[i].classList.remove('reveal_active');
+        }
+    }
 });

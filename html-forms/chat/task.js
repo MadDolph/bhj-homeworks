b634346bd0f728f93a602dedb22620357ac23e3d
MadDolph <kurbatov@it-matika.pro>
2023-03-07 02:26:49 +0300

    make chat

---
+++ html-forms/chat/task.js
@@ -0,0 +1,34 @@
+const chatWidget = document.querySelector(".chat-widget")
+const chatInput = document.getElementById('chat-widget__input');
+const messages = document.querySelector( '.chat-widget__messages');
+let date;
+let randomMessage = ['меня будить????', 
+'а ещё что ты хочешь?', 'приветствую тебя идущий на смерть', 
+'нет вобле', 'груминг хор', 'Люк! Я твой отец!', 'Я хоть и не Алиса, но могу поддержкать разговор!',
+'гуляла бы ты девочка отседа...', 'а в тюрьме сейчас ужин - макароны...'];
+
+chatWidget.addEventListener('click', (e) => {
+    if(e.target.classList == 'chat-widget__side-text') {
+        chatWidget.classList.toggle('chat-widget_active');        
+    }
+})
+
+chatInput.addEventListener('keypress', (e) => {
+    if(e.keyCode == 13 && chatInput.value != '') {
+        console.log(chatInput.value);
+        date = new Date();
+        messages.innerHTML += `
+            <div class="message message_client">
+            <div class="message__time">${('0' + date.getHours()).slice(-2)}:${('0' + date.getMinutes()).slice(-2)}</div>
+            <div class="message__text">${chatInput.value}</div>
+            </div>
+        `;
+        messages.innerHTML += `
+            <div class="message">
+            <div class="message__time">${('0' + date.getHours()).slice(-2)}:${('0' + date.getMinutes()).slice(-2)}</div>
+            <div class="message__text">${randomMessage[Math.round(Math.random() * (randomMessage.length - 0) + 0)]}</div>
+            </div>
+        `;        
+    }
+})
+

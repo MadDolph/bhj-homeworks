2094bae19ba632874803275a57e2e72c2706ab97
MadDolph <kurbatov@it-matika.pro>
2023-03-07 23:59:19 +0300

    checked completed

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,6 +1,18 @@
-const interest = document.querySelectorAll('.interest__check');
-let interestArray = Array.from(interest);
-let rootInterest = [];
-for(el of interestArray) {
-    console.log(el.parentNode.parentNode.parentNode.parentNode);
-}
+const struct = document.querySelectorAll('.interest');
+let structArray = Array.from(struct);
+for(let el of structArray) {
+    if(el.parentElement.className == '') {
+        let branch = el.querySelectorAll('.interest__check');
+        el.querySelector('.interest__check').addEventListener('change', (e) => {
+            if(e.target.checked) {
+                for(let element of branch) {
+                    element.checked = true;
+                }
+            } else {
+                for(let element of branch) {
+                    element.checked = false;
+                } 
+            }
+        })
+    }
+ }

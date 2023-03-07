dfd4c9f5bba60ffbc2f2d3f4f127821cf708218b
MadDolph <kurbatov@it-matika.pro>
2023-03-08 00:08:32 +0300

    some remarks

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -2,14 +2,13 @@ const struct = document.querySelectorAll('.interest');
 let structArray = Array.from(struct);
 for(let el of structArray) {
     if(el.parentElement.className == '') {
-        let branch = el.querySelectorAll('.interest__check');
         el.querySelector('.interest__check').addEventListener('change', (e) => {
             if(e.target.checked) {
-                for(let element of branch) {
+                for(let element of el.querySelectorAll('.interest__check')) {
                     element.checked = true;
                 }
             } else {
-                for(let element of branch) {
+                for(let element of el.querySelectorAll('.interest__check')) {
                     element.checked = false;
                 } 
             }

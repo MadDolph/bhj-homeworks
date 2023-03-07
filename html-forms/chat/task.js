0a2d48d97d2466534a95988acab6b2f88092a3a4
MadDolph <kurbatov@it-matika.pro>
2023-03-07 15:32:44 +0300

    Chat complete

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -7,16 +7,21 @@ let randomMessage = ['меня будить????',
 'а ещё что ты хочешь?', 'приветствую тебя идущий на смерть', 
 'нет вобле', 'груминг хор', 'Люк! Я твой отец!', 'Я хоть и не Алиса, но могу поддержкать разговор!',
 'гуляла бы ты девочка отседа...', 'а в тюрьме сейчас ужин - макароны...'];
-
+let autoAnswer = setInterval(botAnswer, 30000);
 chatWidget.addEventListener('click', (e) => {
     if(e.target.classList == 'chat-widget__side-text') {
-        chatWidget.classList.toggle('chat-widget_active');        
+        chatWidget.classList.toggle('chat-widget_active');
+        date = new Date();
+        messages.innerHTML += `
+            <div class="message">
+            <div class="message__time">${('0' + date.getHours()).slice(-2)}:${('0' + date.getMinutes()).slice(-2)}</div>
+            <div class="message__text">Здравствуйте! Что привело Вас в это - богом забытое место?!</div>
+            </div>
+        `;
     }
 })
-
 chatInput.addEventListener('keypress', (e) => {
     if(e.keyCode == 13 && chatInput.value != '') {
-        console.log(chatInput.value);
         date = new Date();
         messages.innerHTML += `
             <div class="message message_client">
@@ -24,14 +29,21 @@ chatInput.addEventListener('keypress', (e) => {
             <div class="message__text">${chatInput.value}</div>
             </div>
         `;
-        messages.innerHTML += `
-            <div class="message">
-            <div class="message__time">${('0' + date.getHours()).slice(-2)}:${('0' + date.getMinutes()).slice(-2)}</div>
-            <div class="message__text">${randomMessage[Math.round(Math.random() * (randomMessage.length - 0) + 0)]}</div>
-            </div>
-        `;
+        clearInterval(autoAnswer);
+        autoAnswer = setInterval(botAnswer, 30000);
+        botAnswer();        
         chatInput.value = null;
-        chatContainer.scrollTop = chatContainer.scrollHeight;
     }
 })
+
+function botAnswer() {
+    date = new Date();
+    messages.innerHTML += `
+            <div class="message">
+            <div class="message__time">${('0' + date.getHours()).slice(-2)}:${('0' + date.getMinutes()).slice(-2)}</div>
+            <div class="message__text">${randomMessage[Math.trunc(Math.random() * (randomMessage.length - 0) + 0)]}</div>
+            </div>
+        `;
+        chatContainer.scrollTop = chatContainer.scrollHeight;
+}
 

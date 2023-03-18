9aca7a0743f28910b1f3383e4c1c02a37bd7f96c
Kurbatov Vladimir <102817717+MadDolph@users.noreply.github.com>
2023-03-18 21:31:10 +0300

    Update task.js

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,12 +1,23 @@
-const tooltips = document.querySelectorAll('.has-tooltip');
-const links = document.querySelectorAll('.has-tooltip');
-for(let el of tooltips) {
-    el.nTitle = el.title;
-    el.title = '';
+const tooltipHref = Array.from(document.querySelectorAll('a.has-tooltip'));
+const tooltip = document.createElement('div');
+tooltip.classList.add('tooltip');
+
+const showTooltip = function(e) {
+    e.preventDefault();
+    if (tooltipHref.includes(e.target)) {
+            if (e.target.querySelector('.tooltip_active')) {
+                tooltip.classList.remove('tooltip_active');
+                return;
+            }
+        let coordinates = e.target.getBoundingClientRect();
+        tooltip.classList.add('tooltip_active');
+        tooltip.textContent = e.target.title;
+        tooltip.style.top = coordinates.y + coordinates.height + 'px';
+        tooltip.style.left = coordinates.x + 'px';
+        e.target.appendChild(tooltip);
+    } else if (tooltip.classList.contains('tooltip_active')) {
+        tooltip.classList.remove('tooltip_active');
+    }
 }
-for(let el of links) {
-    el.addEventListener('click', (e) => {
-        console.log(e.target.textContent);
-        e.preventDefault();
-    })
-}
+
+document.addEventListener('click', showTooltip)

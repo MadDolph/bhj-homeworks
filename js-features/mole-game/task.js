00455c8cb6dc761841c29a500657558b20e7d378
MadDolph <kurbatov@it-matika.pro>
2023-07-30 21:54:43 +0300

    continue

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,33 +1,34 @@
-const rip = document.getElementById("dead");
-const loose = document.getElementById("lost");
-let kill = parseInt(rip.textContent);
-let fail = parseInt(loose.textContent);
+const holes = document.getElementsByClassName('hole');
+const dead = document.getElementById('dead');
+const loose = document.getElementById('lost');
+let kills = 0;
+let miss = 0;
+
 function setDefault() {
-    kill = 0;
-    fail = 0;
-    rip.textContent = kill;
-    loose.textContent = fail;
+    kills = 0;
+    miss = 0;
+    dead.textContent = kills;
+    loose.textContent = miss;
 }
-function getHole(index) {
-    let holename = "hole" + index;
-    return(document.getElementById(holename));
-}
-for(let i = 1; i < 10; i++) {
-    getHole(i).onclick = () => {
-        if(getHole(i).classList.contains("hole_has-mole")) {
-            kill++;      
-            rip.textContent = kill;
-            if(kill > 9) {
-                setDefault();
-                window.alert("Вы выиграли");
+
+for (let i = 0; i < holes.length; i++) {
+    holes[i].onclick = function() {
+        if(holes[i].classList.contains('hole_has-mole')) {
+            kills++;
+            if(kills < 10) {
+                dead.textContent = kills;
+            } else {
+                setDefault();                
+                alert("Вы выиграли!")
             }
         } else {
-            fail++;           
-            loose.textContent = fail;
-            if(fail > 4) {
-                setDefault();
-                window.alert("Вы проиграли");
-            }
-         }             
-    }    
+           miss++;
+           if(miss < 5) {
+                loose.textContent = miss;                
+           } else {
+            setDefault();                
+            alert("Вы проиграли!")
+           }
+        }
+    }
 }
